a91996a6d683e59793fa749df206388745a27fa7
Felix Jumason <felixkent360@gmail.com>
2023-10-14 12:50:38 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+           
             {children}
           </ThemeProvider></body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
-
+import Nav from '../app/(dashboard)/layout'
 export default function Home() {
   return (
    <div>
+    <Nav />
       <h1>heloo</h1>
    </div>
   )
