82f0deefe84126dca5669add37d88e6e1f0fe8bc
Felix Jumason <felixkent360@gmail.com>
2023-10-14 15:37:17 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { Suspense } from 'react';
 import Layout from '@/app/(dashboard)/layout'
 import { GetFormStats } from '@/actions/form';
 import { LuView } from 'react-icons/lu';
+import { FaWpforms } from 'react-icons/fa';
+import { HiCursorClick } from 'react-icons/hi';
+import { TbArrowBounce } from 'react-icons/tb';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
@@ -9,7 +12,7 @@ export default function Home() {
   return (
     <div className='container '>
       <Layout />
-      <Suspense>
+      <Suspense fallback={<StatsCard loading={true} />}>
         <CardStatsWrapper />
       </Suspense>
     </div>
@@ -22,7 +25,7 @@ async function CardStatsWrapper() {
 }
 
 interface StatsCardProps {
-  data: Awaited<ReturnType<typeof GetFormStats>>;
+  data?: Awaited<ReturnType<typeof GetFormStats>>;
   loading: boolean;
 }
 
@@ -35,9 +38,33 @@ function StatsCard(props: StatsCardProps) {
         title='Total Visits'
         icon={<LuView className='text-blue-600' />}
         helperText="All time Form Visits"
-        value={data.visits.toLocaleString()}
+        value={data?.visits.toLocaleString() || ""}
         loading={loading}
         className='shadow-md shadow-blue-600'
+      />
+      <SingleStatsCard
+        title='Total Submissions'
+        icon={<FaWpforms className='text-yellow-600' />}
+        helperText="All time Form Submissions"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className='shadow-md shadow-orange-600'
+      />
+      <SingleStatsCard
+        title='Submission Rate '
+        icon={<HiCursorClick className='text-green-600' />}
+        helperText="Visits that results in form submission"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className='shadow-md shadow-green-600'
+      />
+      <SingleStatsCard
+        title='Bounce Rate '
+        icon={<TbArrowBounce className='text-purple-600' />}
+        helperText="Visits with out interaction"
+        value={data?.visits.toLocaleString() || ""}
+        loading={loading}
+        className='shadow-md shadow-purple-600'
       />
     </div>
   );
@@ -60,14 +87,19 @@ function SingleStatsCard({
 }) {
   return (
     <Card className={className}>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
+      <CardHeader className='flex flex-row items-center justify-between pb-2'>
+        <CardTitle className='text-sm font-sans text-muted-foreground'>{title}</CardTitle>
         {icon}
       </CardHeader>
       <CardContent>
         <div className='text-2xl font-bold'>
-          {loading && <Skeleton><span>0</span></Skeleton>}
+          {loading && (<Skeleton>
+            <span className='opacity-0'>0</span>
+            </Skeleton>
+            )}
+            {!loading && value}
         </div>
+        <p className='text-xs text-muted-foreground pt-1'> {helperText}</p>
       </CardContent>
     </Card>
   );
