1674669b5a0f113c744b6519e8254b93d05a16f6
Felix Jumason <felixkent360@gmail.com>
2023-11-03 02:45:32 +0300

    update

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,19 +1,19 @@
-import { ElementsType, FormElementInstance } from "@/components/FormElements";
-import FormLinkShare from "@/components/FormLinkShare ";
-import VisitBtn from "@/components/VisitBtn ";
-import { Badge } from "@/components/ui/badge";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { format, formatDistance } from "date-fns";
-import { ReactNode } from "react";
+import { GetFormById, GetFormWithSubmissions } from "@/actions/form";
+import React, { ReactNode } from "react";
+import { LuView } from "react-icons/lu";
 import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
-import { LuView } from "react-icons/lu";
 import { TbArrowBounce } from "react-icons/tb";
+import { ElementsType, FormElementInstance } from "@/components/FormElements";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { format, formatDistance } from "date-fns";
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+import VisitBtn from "@/components/VisitBtn ";
+import FormLinkShare from "@/components/FormLinkShare ";
 import { SingleStatsCard } from "../../page";
-import { GetFormById, GetFormWithSubmissions } from "@/actions/form";
 
-async function FormDetails({
+async function FormDetailPage({
   params,
 }: {
   params: {
@@ -26,80 +26,83 @@ async function FormDetails({
     throw new Error("form not found");
   }
 
-  
   const { visits, submissions } = form;
- let submissionsRate =  0;
- 
- if (visits > 0){
-  submissionsRate = (submissions / visits) * 100;
- }
- const bounceRate = 100 - submissionsRate;
 
-  return(
+  let submissionRate = 0;
+
+  if (visits > 0) {
+    submissionRate = (submissions / visits) * 100;
+  }
+
+  const bounceRate = 100 - submissionRate;
+
+  return (
     <>
-    <div className="py-10 border-b border-muted">
-      <div className="flex justify-between container">
-        <h1 className="text-4xl font-bold truncate">{form.name}</h1>
-        <VisitBtn shareUrl={form.shareURL} />
-      </div>
+      <div className="py-10 border-b border-muted">
+        <div className="flex justify-between container">
+          <h1 className="text-4xl font-bold truncate">{form.name}</h1>
+          <VisitBtn shareUrl={form.shareURL} />
+        </div>
       </div>
       <div className="py-4 border-b border-muted">
         <div className="container flex gap-2 items-center justify-between">
-         <div className="container flex gap-2 items-center justify-between">
-         <FormLinkShare shareUrl={form.shareURL} />
-         </div>
+          <FormLinkShare shareUrl={form.shareURL} />
         </div>
       </div>
       <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container">
-      <SingleStatsCard
-        title='Total Visits'
-        icon={<LuView className='text-blue-600' />}
-        helperText="All time Form Visits"
-        value={visits.toLocaleString() || ""}
-        loading={false}
-        className='shadow-md shadow-blue-600'
-      />
-      <SingleStatsCard
-        title='Total Submissions'
-        icon={<FaWpforms className='text-yellow-600' />}
-        helperText="All time Form Submissions"
-        value={visits.toLocaleString() || ""}
-        loading={false}
-        className='shadow-md shadow-orange-600'
-      />
-      <SingleStatsCard
-        title='Submission Rate '
-        icon={<HiCursorClick className='text-green-600' />}
-        helperText="Visits that results in form submission"
-        value={visits.toLocaleString() + "%" || ""}
-        loading={false}
-        className='shadow-md shadow-green-600'
-      />
-      <SingleStatsCard
-        title='Bounce Rate '
-        icon={<TbArrowBounce className='text-purple-600' />}
-        helperText="Visits with out interaction"
-        value={visits.toLocaleString() + "%"|| ""}
-        loading={false}
-        className='shadow-md shadow-purple-600'
-      />
-         </div>
-         <div className="container pt-10">
-          <SubmissionsTable id={form.id} />
-         </div>
+        <SingleStatsCard
+          title="Total visits"
+          icon={<LuView className="text-blue-600" />}
+          helperText="All time form visits"
+          value={visits.toLocaleString() || ""}
+          loading={false}
+          className="shadow-md shadow-blue-600"
+        />
 
-    
+        <SingleStatsCard
+          title="Total submissions"
+          icon={<FaWpforms className="text-yellow-600" />}
+          helperText="All time form submissions"
+          value={submissions.toLocaleString() || ""}
+          loading={false}
+          className="shadow-md shadow-yellow-600"
+        />
+
+        <SingleStatsCard
+          title="Submission rate"
+          icon={<HiCursorClick className="text-green-600" />}
+          helperText="Visits that result in form submission"
+          value={submissionRate.toLocaleString() + "%" || ""}
+          loading={false}
+          className="shadow-md shadow-green-600"
+        />
+
+        <SingleStatsCard
+          title="Bounce rate"
+          icon={<TbArrowBounce className="text-red-600" />}
+          helperText="Visits that leaves without interacting"
+          value={bounceRate.toLocaleString() + "%" || ""}
+          loading={false}
+          className="shadow-md shadow-red-600"
+        />
+      </div>
+
+      <div className="container pt-10">
+        <SubmissionsTable id={form.id} />
+      </div>
     </>
   );
 }
 
-export default FormDetails;
+export default FormDetailPage;
 
-type Row = {[key: string]: string} & {
-   submittedAt: Date;};
+type Row = { [key: string]: string } & {
+  submittedAt: Date;
+};
 
- async function SubmissionsTable ({id}: {id: number}) {
+async function SubmissionsTable({ id }: { id: number }) {
   const form = await GetFormWithSubmissions(id);
+
   if (!form) {
     throw new Error("form not found");
   }
@@ -109,9 +112,10 @@ type Row = {[key: string]: string} & {
     id: string;
     label: string;
     required: boolean;
-    type: ElementsType
+    type: ElementsType;
   }[] = [];
-  formElements.forEach(element => {
+
+  formElements.forEach((element) => {
     switch (element.type) {
       case "TextField":
       case "NumberField":
@@ -126,61 +130,55 @@ type Row = {[key: string]: string} & {
           type: element.type,
         });
         break;
-        default:
+      default:
         break;
     }
   });
 
   const rows: Row[] = [];
-  form.FormSubmissions.forEach(submission => {
+  form.FormSubmissions.forEach((submission) => {
     const content = JSON.parse(submission.content);
     rows.push({
       ...content,
       submittedAt: submission.createdAt,
-    })
-  })
+    });
+  });
+
   return (
     <>
-    <h1 className="text-2xl font-bold my-4">Submissions</h1>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            {columns.map((column) => (
-              <TableHead key={column.id} className="uppercase">
-                {column.label}
-              
-              </TableHead>
-            ))}
-            <TableHead className="text-muted-foreground text-right uppercase">
-              Submitted at
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-                {rows.map((row, index) => (
-                  <TableRow key={index}>
-                    {columns.map((column) => (
-                      <RowCell
-                      key={column.id}
-                      type={column.type}
-                      value={row[column.id]}
-                      />
-                    ))}
-                    <TableCell className="text-muted-foreground text-right">
-                      {formatDistance(new Date(row.submittedAt), new Date(), {
-                        addSuffix: true,
-                      })}
-                      
-                    </TableCell>
-                  </TableRow>
+      <h1 className="text-2xl font-bold my-4">Submissions</h1>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column) => (
+                <TableHead key={column.id} className="uppercase">
+                  {column.label}
+                </TableHead>
+              ))}
+              <TableHead className="text-muted-foreground text-right uppercase">Submitted at</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index}>
+                {columns.map((column) => (
+                  <RowCell key={column.id} type={column.type} value={row[column.id]} />
                 ))}
-        </TableBody>
-      </Table>
-    </div>
+                <TableCell className="text-muted-foreground text-right">
+                  {formatDistance(row.submittedAt, new Date(), {
+                    addSuffix: true,
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </>
   );
 }
+
 function RowCell({ type, value }: { type: ElementsType; value: string }) {
   let node: ReactNode = value;
 
